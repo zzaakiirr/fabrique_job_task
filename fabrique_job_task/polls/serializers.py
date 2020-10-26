34ca61b4947e751263f3b3ffd2cefca784b754a1
zzaakiirr <pfrbhxbr@gmail.com>
2020-10-26 19:34:32 +0300

    Added validation for end_date field in PollSerializer

--- fabrique_job_task/polls/serializers.py
+++ fabrique_job_task/polls/serializers.py
@@ -1,11 +1,24 @@
+from datetime import datetime as dt
+
 from rest_framework import serializers
 
 from .models import Poll, Question, Answer
-
 from .serializer_validators import user_existence_validator
 
 
 class PollCreateSerializer(serializers.ModelSerializer):
+    desc = serializers.CharField(allow_blank=True, required=False)
+    start_date = serializers.DateField()
+    end_date = serializers.DateField()
+
+    def validate_end_date(self, value):
+        start_date = dt.strptime(
+            self.initial_data.get('start_date'),
+            "%Y-%m-%d"
+        )
+        if start_date and start_date.date() > value:
+            raise serializers.ValidationError("End date should be after start")
+        return value
 
     class Meta:
         model = Poll
